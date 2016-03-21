ab19cd5b3b627c494b2386177da79e4b1e03f320
Gmason <gmason822@gmail.com>
2016-03-21 16:07:10 -0700

    Added text fields and submit button to edit profile. Added mock up of profile info on profile page

--- App/app/src/main/java/com/example/jennifer/floatapp/EditProfile.java
+++ App/app/src/main/java/com/example/jennifer/floatapp/EditProfile.java
@@ -1,7 +1,9 @@
 package com.example.jennifer.floatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
     }
+    public void submit_pressed(View v){
+
+        startActivity(new Intent(this, ProfileActivity.class));
+    }
+
 }
--- App/app/src/main/java/com/example/jennifer/floatapp/ProfileActivity.java
+++ App/app/src/main/java/com/example/jennifer/floatapp/ProfileActivity.java
@@ -16,17 +16,8 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-    public void logoutPressed(View v){startActivity(new Intent(this, LoginActivity.class));}
+    public void logout_pressed(View v){startActivity(new Intent(this, LoginActivity.class));}
 
-    public void editPressed(View v){startActivity(new Intent(this, EditProfile.class));}
+    public void edit_profile_pressed(View v){startActivity(new Intent(this, EditProfile.class));}
 }
